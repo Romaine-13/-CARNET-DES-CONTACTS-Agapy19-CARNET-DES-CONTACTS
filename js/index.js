5586a98f8d6f2206774a64e7c40b911d00e38321
Jerry LUNDA <jerrylundakasongo@gmail.com>
2023-11-16 15:19:47 +0200

    initialize variables

---
+++ js/index.js
@@ -0,0 +1,38 @@
+//initialize array of contact persons
+let array_contacts = [];
+
+/**
+ * function to validate content
+ * if return is -1: length of name is < 3
+ * if return is =1: length of name is between 3 and 50
+ * if return is 0: length of name is between 3 and 50
+ * 
+ * @param {string} param_name 
+ */
+function validName(param_name) {
+    if(param_name.length>=3 && param_name.length<=50){
+        
+    }
+}
+
+//creation and initilize contact object
+let contact = {
+    _prenom:"",
+    _prenomnom:"",
+    _telephone:"",
+    _groupe:"",
+    _email:"",
+    _bio:"",
+    _picture:"",
+
+    set prenom(pPrenom){
+
+    },
+
+    set nom(pPrenom){
+
+    },
+};
+
+
+
